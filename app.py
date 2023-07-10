fbbbac1782fe86bb1478984b19e2e2c67283715b
shelabd <s.elabd@live.com>
2023-07-10 21:50:35 +0000

    Commit changes made to app

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-import Flask
+from flask import Flask
+from flask import json
 import logging
-
 
 app = Flask(__name__)
 
@@ -18,7 +18,9 @@ def healthcheck():
 @app.route('/metrics')
 def metrics():
     response = app.response_class(
-            response=json.dumps({"status":"success","code":0,"data":{"UserCount":140,"UserCountActive":23}}),
+            response=json.dumps(
+            {"status":"success","code":0,"data":{"UserCount":140,"UserCountActive":23}}
+            ),
             status=200,
             mimetype='application/json'
     )
@@ -34,5 +36,4 @@ def hello():
 if __name__ == "__main__":
     ## stream logs to a file
     logging.basicConfig(filename='app.log',level=logging.DEBUG)
-    
     app.run(host='0.0.0.0')
